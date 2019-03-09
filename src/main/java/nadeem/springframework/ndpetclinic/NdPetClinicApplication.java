75076831cc4ac6cf3453444564d2beba9b90d549
Nadeem22 <nadeem2107@gmail.com>
2019-03-10 00:48:03 +0530

    initial git commit

---
+++ src/main/java/nadeem/springframework/ndpetclinic/NdPetClinicApplication.java
@@ -0,0 +1,13 @@
+package nadeem.springframework.ndpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NdPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NdPetClinicApplication.class, args);
+    }
+
+}
