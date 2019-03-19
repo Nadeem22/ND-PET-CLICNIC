93019b2a8a652d26009f8d5031ac84ae69bc6310
Nadeem22 <nadeem2107@gmail.com>
2019-03-19 23:32:55 +0530

    adding Index and Controller, Closes #8

--- pet-clinic-web/src/test/java/ndpetclinic/NdPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/ndpetclinic/NdPetClinicApplicationTests.java
@@ -2,11 +2,11 @@ package ndpetclinic;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootConfiguration
 public class NdPetClinicApplicationTests {
 
     @Test
