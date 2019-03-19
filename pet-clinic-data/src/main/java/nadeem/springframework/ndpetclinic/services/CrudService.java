429ddd7d268f63f3a410d5fc5fc3e46c32d92d18
Nadeem22 <nadeem2107@gmail.com>
2019-03-19 23:00:59 +0530

    implements Map Based Services, Closes #6

--- pet-clinic-data/src/main/java/nadeem/springframework/ndpetclinic/services/CrudService.java
+++ pet-clinic-data/src/main/java/nadeem/springframework/ndpetclinic/services/CrudService.java
@@ -4,9 +4,13 @@ import java.util.Set;
 
 public interface CrudService<T, ID> {
     Set<T> findAll();
+
     T findById(ID id);
-   T save(T oject);
-   void delete(T object);
-   void deleteById(ID id);
+
+    T save(T oject);
+
+    void delete(T object);
+
+    void deleteById(ID id);
 
 }
