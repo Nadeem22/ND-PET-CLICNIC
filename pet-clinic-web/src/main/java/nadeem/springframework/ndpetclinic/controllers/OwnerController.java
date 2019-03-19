c92b108ff054bf75c58fa999779cea5a37679ba0
Nadeem22 <nadeem2107@gmail.com>
2019-03-20 00:31:46 +0530

    Creating index page and Controller for Owner , Closes #13

---
+++ pet-clinic-web/src/main/java/nadeem/springframework/ndpetclinic/controllers/OwnerController.java
@@ -0,0 +1,13 @@
+package nadeem.springframework.ndpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+    @RequestMapping({"","/","/index","/index.html"})
+    public String listOwners(){
+
+        return "owners/index";
+    }
+}
