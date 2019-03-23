7e3d737ee9b73f2a65b242974cbbcc60bc1a6a57
Nadeem22 <nadeem2107@gmail.com>
2019-03-23 09:05:51 +0530

    Added Specialites to Map Service Closes #32

---
+++ pet-clinic-data/src/main/java/nadeem/springframework/ndpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,7 @@
+package nadeem.springframework.ndpetclinic.services;
+
+import nadeem.springframework.ndpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality,Long> {
+
+}
